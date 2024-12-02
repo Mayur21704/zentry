c6feb0cf79a06d48faff196dbc53924d8cd5792a
Mayur <mayur72104@gmail.com>
2024-12-02 15:59:33 +0530

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import { FaGithub, FaGoogle, FaTwitch, FaTwitter } from "react-icons/fa";
+
+const links = [
+  { href: "https://google.com", icon: <FaGoogle /> },
+  { href: "https://x.com", icon: <FaTwitter /> },
+  { href: "https://github.com", icon: <FaGithub /> },
+  { href: "https://twitch.com", icon: <FaTwitch /> },
+];
+
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-violet-300 py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-10 md:flex-row ">
+        <p className="text-center text-sm font-light md:text-left">
+          ©Nova 2024. All rights reserved
+        </p>
+
+        <div className="flex justify-center gap-4 md:justify-start">
+          {links.map((link) => (
+            <a
+              key={link.icon}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+
+        <a
+          href="#privacy-policy"
+          className="text-center text-sm hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
